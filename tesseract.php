424fb9d8683216b9c52c0ba8f717ac81fffd7a85
JrFernando <fernandojunior20110@gmail.com>
2018-01-10 10:20:41 -0200

    Adição do arquivo que executa o tesseract na imagem

---
+++ tesseract.php
@@ -0,0 +1,32 @@
+<?php
+  require_once "vendor/autoload.php";
+
+  use thiagoalessio\TesseractOCR\TesseractOCR;
+
+  // echo "<head><meta charset='utf-8'/></head>";
+
+  if(isset($_POST['pathFile'])){
+    //Obtem nome do arquivo com extensão
+    $name = explode('/', $_POST['pathFile']);
+    $name =  $name[1];
+
+    //Retira a extensão
+    $name = explode('.', $name);
+    $name =  $name[0];
+
+    //Adiciona pasta e .txt
+    $name = "resultTesseract/" . $name . ".txt";
+
+    echo $name;
+
+    $file = fopen($name, "w");
+
+    $result = (new TesseractOCR($_POST['pathFile']))
+      ->lang("por", "eng")
+      ->run();
+
+    fwrite($file, $result);
+    fclose($file);
+
+    echo "Arquivo salvo com sucesso!";
+  }
