9ab236e4e3e193a156c05f3b124f7513d8a900a6
JrFernando <fernandojunior20110@gmail.com>
2018-01-30 18:38:36 -0200

    Busca as informações da molécula no arquivo xlsx.

---
+++ new_find.php
@@ -0,0 +1,45 @@
+<?php
+  require_once __DIR__ . '/util/simplexlsx.class.php';
+
+  define("BD", "COSMETICS_Inventory.xlsx");
+  // define("BD", "bd_debug.xlsx");
+
+  if(isset($_POST['pathFile'])){
+    $pathFile = $_POST['pathFile'];
+    $resutados = NULL;
+
+    //Abertura do arquivo retornado pelo Tesseract
+    $file = fopen($pathFile, "r");
+
+    // Obtém linha por linha do arquivo
+    while(($string = fgets($file)) !== false){
+      //Remove new line
+      $string = trim(preg_replace("/\r|\n/", "", $string));
+      if(strcasecmp($string, "") == 0){
+        //Linha Vazia
+      } else{
+
+        //Função que busca a linha na base de dados
+        $resp = findOnBD($string);
+
+        //Se retornar um resulado válido é adicionado ao array de resultados
+        if(! is_null($resp))
+          $resutados[] = $resp;
+      }
+    }
+    echo json_encode($resutados);
+  } else {
+    echo "Erro!";
+  }
+
+
+  function findOnBD(String $string){
+
+    if($xlsx = SimpleXLSX::parse(BD)){
+      foreach ($xlsx->rows() as $line) {
+         if(strcasecmp($line[4], $string) == 0 || strcasecmp($line[5], $string) == 0){
+           return $line;
+         }
+      }
+    }
+  }
